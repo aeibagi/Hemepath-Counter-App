95dbfbaa507c9f4c8681c8aac70abaa6da664a5a
jakekarnes42 <jake.karnes@gmail.com>
2013-03-06 11:50:11 -0800

    Added info icon to Action bar and added About dialog functionality

--- src/edu/sjsu/hemepathcounter/CountersActivity.java
+++ src/edu/sjsu/hemepathcounter/CountersActivity.java
@@ -1,8 +1,7 @@
 package edu.sjsu.hemepathcounter;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.sax.TextElementListener;
+import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
@@ -11,70 +10,71 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
-public class CountersActivity extends Activity implements View.OnClickListener
-{
+/**
+ * 
+ * @author Amir Eibagi
+ * 
+ */
+public class CountersActivity extends Activity implements View.OnClickListener {
 	private Button mainMenuButton, backButton;
 	private ListView CountersList;
 	private ArrayAdapter<String> CounterListAdaptor;
 	private EditText CountersSearchBox;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_counters);
 		initialize();
-	    SetupSearchFilter();
-		
+		SetupSearchFilter();
+
 	}
 
-
-
 	private void initialize() {
-		//initialize the button and setup actionlisteners
+		// initialize the button and setup actionlisteners
 		mainMenuButton = (Button) findViewById(R.id.CountersActivity_mainMenu_button);
 		backButton = (Button) findViewById(R.id.CountersActivity_back_button);
 		mainMenuButton.setOnClickListener(this);
 		backButton.setOnClickListener(this);
-		//initialize the Search box
+		// initialize the Search box
 		CountersSearchBox = (EditText) findViewById(R.id.EditBox_Counters_Search);
 		// setting up the list for the counters
 		CountersList = (ListView) findViewById(R.id.Counters_list);
-		CounterListAdaptor = new ArrayAdapter<String>(this, R.layout.counters_list_item, R.id.TextView_Counters_List_items);
+		CounterListAdaptor = new ArrayAdapter<String>(this,
+				R.layout.counters_list_item, R.id.TextView_Counters_List_items);
 		CounterListAdaptor.add("Counter 1");
 		CounterListAdaptor.add("Counter 2");
 		CounterListAdaptor.add("Counter 3");
 		CounterListAdaptor.add("Counter 4");
 		CountersList.setAdapter(CounterListAdaptor);
 	}
-	
-	private void SetupSearchFilter() 
-	{
+
+	private void SetupSearchFilter() {
 		CountersSearchBox.addTextChangedListener(new TextWatcher() {
-			
+
 			@Override
-			public void onTextChanged(CharSequence s, int start, int before, int count)
-			{
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
 				CountersActivity.this.CounterListAdaptor.getFilter().filter(s);
 			}
-			
+
 			@Override
 			public void beforeTextChanged(CharSequence s, int start, int count,
 					int after) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void afterTextChanged(Editable s) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
-		
+
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -82,19 +82,15 @@ public class CountersActivity extends Activity implements View.OnClickListener
 		return true;
 	}
 
-
-
 	@Override
-	public void onClick(View v) 
-	{
-		switch(v.getId())
-		{
-			case R.id.CountersActivity_mainMenu_button:
-				break;
-			case R.id.CountersActivity_back_button:
-				break;
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.CountersActivity_mainMenu_button:
+			break;
+		case R.id.CountersActivity_back_button:
+			break;
 		}
-		
+
 	}
 
 }
--- src/edu/sjsu/hemepathcounter/MainActivity.java
+++ src/edu/sjsu/hemepathcounter/MainActivity.java
@@ -1,15 +1,15 @@
 package edu.sjsu.hemepathcounter;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
 
 /**
  * This is the application's default landing page or main menu. The user will
@@ -21,9 +21,8 @@ import android.widget.AdapterView.OnItemClickListener;
  * @author Jake Karnes
  * 
  */
-public class MainActivity extends Activity implements OnItemClickListener,
-                                            View.OnClickListener{
-	
+public class MainActivity extends Activity implements View.OnClickListener {
+
 	private ListView favoritesListView;
 	private ArrayAdapter<String> adapter;
 	private Button counterButton, newButton, preferenceButton, dataButton;
@@ -33,8 +32,9 @@ public class MainActivity extends Activity implements OnItemClickListener,
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		initialize();
-		
-		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1);
 		adapter.add("Favorite 1");
 		adapter.add("Favorite 2");
 		adapter.add("Favorite 3");
@@ -44,48 +44,58 @@ public class MainActivity extends Activity implements OnItemClickListener,
 
 	private void initialize() {
 		favoritesListView = (ListView) findViewById(R.id.favorites_list);
-		//initialize our button on the main activity
+		// initialize our button on the main activity
 		counterButton = (Button) findViewById(R.id.counters_button);
 		newButton = (Button) findViewById(R.id.new_button);
 		preferenceButton = (Button) findViewById(R.id.preferences_button);
 		dataButton = (Button) findViewById(R.id.data_button);
-		// setting up click action listiners for each buttons
+		// setting up click action listeners for each buttons
 		counterButton.setOnClickListener(this);
 		newButton.setOnClickListener(this);
 		preferenceButton.setOnClickListener(this);
-		dataButton.setOnClickListener(this);	
+		dataButton.setOnClickListener(this);
 	}
 
+	/**
+	 * Create a menu to be displayed when user hits Menu key on device
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main, menu);
+	    return true;
 	}
 
+	/** Menu Item Click Listener */
 	@Override
-	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-		// TODO Auto-generated method stub
-		
-	}
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.menu_options_about:
+			DialogPrompt.showAppAboutDialog(this);
+			return true;
 
-	@Override
-	public void onClick(View v) 
-	{	// handler for click listener
-		Intent intent;
-		switch(v.getId())
-		{
-			case R.id.counters_button:
-			    intent = new Intent(MainActivity.this, CountersActivity.class);
-				startActivity(intent);
-				break;
-			case R.id.new_button:
-				break;
-			case R.id.preferences_button:
-				break;
-			case R.id.data_button:
-				break;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-    }
+
+	}
+
+	@Override
+	public void onClick(View v) { // handler for click listener
+		Intent intent;
+		switch (v.getId()) {
+		case R.id.counters_button:
+			intent = new Intent(MainActivity.this, CountersActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.new_button:
+			break;
+		case R.id.preferences_button:
+			break;
+		case R.id.data_button:
+			break;
+		}
+	}
 
 }
