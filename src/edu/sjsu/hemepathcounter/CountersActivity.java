8b60639f79f29d5cb388f0ab0fe7d6f9373aa234
Amir Eibagi <amir00018@gmail.com>
2013-03-06 01:03:28 -0800

    Modified MainActivity to include button actionListener and also added countersActivity with search filter functionality

---
+++ src/edu/sjsu/hemepathcounter/CountersActivity.java
@@ -0,0 +1,100 @@
+package edu.sjsu.hemepathcounter;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.sax.TextElementListener;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class CountersActivity extends Activity implements View.OnClickListener
+{
+	private Button mainMenuButton, backButton;
+	private ListView CountersList;
+	private ArrayAdapter<String> CounterListAdaptor;
+	private EditText CountersSearchBox;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_counters);
+		initialize();
+	    SetupSearchFilter();
+		
+	}
+
+
+
+	private void initialize() {
+		//initialize the button and setup actionlisteners
+		mainMenuButton = (Button) findViewById(R.id.CountersActivity_mainMenu_button);
+		backButton = (Button) findViewById(R.id.CountersActivity_back_button);
+		mainMenuButton.setOnClickListener(this);
+		backButton.setOnClickListener(this);
+		//initialize the Search box
+		CountersSearchBox = (EditText) findViewById(R.id.EditBox_Counters_Search);
+		// setting up the list for the counters
+		CountersList = (ListView) findViewById(R.id.Counters_list);
+		CounterListAdaptor = new ArrayAdapter<String>(this, R.layout.counters_list_item, R.id.TextView_Counters_List_items);
+		CounterListAdaptor.add("Counter 1");
+		CounterListAdaptor.add("Counter 2");
+		CounterListAdaptor.add("Counter 3");
+		CounterListAdaptor.add("Counter 4");
+		CountersList.setAdapter(CounterListAdaptor);
+	}
+	
+	private void SetupSearchFilter() 
+	{
+		CountersSearchBox.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count)
+			{
+				CountersActivity.this.CounterListAdaptor.getFilter().filter(s);
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.counters, menu);
+		return true;
+	}
+
+
+
+	@Override
+	public void onClick(View v) 
+	{
+		switch(v.getId())
+		{
+			case R.id.CountersActivity_mainMenu_button:
+				break;
+			case R.id.CountersActivity_back_button:
+				break;
+		}
+		
+	}
+
+}
--- src/edu/sjsu/hemepathcounter/MainActivity.java
+++ src/edu/sjsu/hemepathcounter/MainActivity.java
@@ -2,10 +2,12 @@ package edu.sjsu.hemepathcounter;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 
@@ -19,22 +21,39 @@ import android.widget.AdapterView.OnItemClickListener;
  * @author Jake Karnes
  * 
  */
-public class MainActivity extends Activity implements OnItemClickListener{
+public class MainActivity extends Activity implements OnItemClickListener,
+                                            View.OnClickListener{
 	
 	private ListView favoritesListView;
 	private ArrayAdapter<String> adapter;
+	private Button counterButton, newButton, preferenceButton, dataButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		favoritesListView = (ListView) findViewById(R.id.favorites_list);
+		initialize();
+		
 		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
 		adapter.add("Favorite 1");
 		adapter.add("Favorite 2");
 		adapter.add("Favorite 3");
 		adapter.add("Favorite 4");
 		favoritesListView.setAdapter(adapter);
+	}
+
+	private void initialize() {
+		favoritesListView = (ListView) findViewById(R.id.favorites_list);
+		//initialize our button on the main activity
+		counterButton = (Button) findViewById(R.id.counters_button);
+		newButton = (Button) findViewById(R.id.new_button);
+		preferenceButton = (Button) findViewById(R.id.preferences_button);
+		dataButton = (Button) findViewById(R.id.data_button);
+		// setting up click action listiners for each buttons
+		counterButton.setOnClickListener(this);
+		newButton.setOnClickListener(this);
+		preferenceButton.setOnClickListener(this);
+		dataButton.setOnClickListener(this);	
 	}
 
 	@Override
@@ -49,5 +68,24 @@ public class MainActivity extends Activity implements OnItemClickListener{
 		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void onClick(View v) 
+	{	// handler for click listener
+		Intent intent;
+		switch(v.getId())
+		{
+			case R.id.counters_button:
+			    intent = new Intent(MainActivity.this, CountersActivity.class);
+				startActivity(intent);
+				break;
+			case R.id.new_button:
+				break;
+			case R.id.preferences_button:
+				break;
+			case R.id.data_button:
+				break;
+		}
+    }
 
 }
