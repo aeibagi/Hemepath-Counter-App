22e87f052ec63c7e65ea3a8b3f7dfb6b896bb830
jakekarnes42 <jake.karnes@gmail.com>
2013-03-19 12:53:39 -0700

    New background for CountingActivity and hooked up preferences

--- src/edu/sjsu/hemepathcounter/CountingActivity.java
+++ src/edu/sjsu/hemepathcounter/CountingActivity.java
@@ -3,7 +3,9 @@ package edu.sjsu.hemepathcounter;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,34 +21,42 @@ public class CountingActivity extends Activity {
 	private Counter mData;
 	private CountingAdapter mAdapter;
 	private ArrayList<Integer> mSequence;
-	
+	private boolean muted;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_counting);
-		
-		//Loading data from database depends New Counting or Favorites
+		muted = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(
+				PreferencesActivity.MUTE, false);
+		// Loading data from database depends New Counting or Favorites
 		mData = getIntent().getParcelableExtra("counter");
 		mAdapter = new CountingAdapter(this, mData);
 		mSequence = new ArrayList<Integer>();
-		
+
 		GridView gridview = (GridView) findViewById(R.id.gridview);
-	    gridview.setAdapter(mAdapter);
-	    
-	    gridview.setOnItemClickListener(new OnItemClickListener() {
-	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-	            mData.getCells().get(position).incrementCount();
-	            mData.incrementTotal();
-	            mSequence.add(position);
-	            mAdapter.notifyDataSetChanged();     
-	            //play sound ....
-	            
-	        }
-	    });	    
-	        
-	    //Undo button
-	    Button btnUndo = (Button) findViewById(R.id.counting_activity_undo);
-	    btnUndo.setOnClickListener(new OnClickListener() {			
+		gridview.setAdapter(mAdapter);
+
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
+				// play sound ....
+				if (!muted) {
+					MediaPlayer player = MediaPlayer.create(
+							CountingActivity.this,
+							mData.getCells().get(position).getSound());
+					player.start();
+				}
+				mData.getCells().get(position).incrementCount();
+				mData.incrementTotal();
+				mSequence.add(position);
+				mAdapter.notifyDataSetChanged();
+			}
+		});
+
+		// Undo button
+		Button btnUndo = (Button) findViewById(R.id.counting_activity_undo);
+		btnUndo.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				if (mSequence.size() > 0) {
@@ -54,17 +64,17 @@ public class CountingActivity extends Activity {
 					mSequence.remove(mSequence.size() - 1);
 					mData.getCells().get(position).decrementCount();
 					mData.decrementTotal();
-		            mAdapter.notifyDataSetChanged();
+					mAdapter.notifyDataSetChanged();
 				}
 			}
 		});
-	    
-	    //Save button
-	    Button btnSave = (Button) findViewById(R.id.counting_activity_save);
-	    btnSave.setOnClickListener(new OnClickListener() {			
+
+		// Save button
+		Button btnSave = (Button) findViewById(R.id.counting_activity_save);
+		btnSave.setOnClickListener(new OnClickListener() {
 			@Override
-			public void onClick(View v) {			
-				//Save mData to file. 
+			public void onClick(View v) {
+				// Save mData to file.
 			}
 		});
 	}
--- src/edu/sjsu/hemepathcounter/adapter/CountingAdapter.java
+++ src/edu/sjsu/hemepathcounter/adapter/CountingAdapter.java
@@ -3,73 +3,96 @@ package edu.sjsu.hemepathcounter.adapter;
 import java.text.DecimalFormat;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import edu.sjsu.hemepathcounter.PreferencesActivity;
 import edu.sjsu.hemepathcounter.R;
 import edu.sjsu.hemepathcounter.model.CellButton;
 import edu.sjsu.hemepathcounter.model.Counter;
 
 public class CountingAdapter extends BaseAdapter {
 
+	private SharedPreferences preferences;
+	private boolean showNumbers;
+	private boolean showPercents;
+	private boolean muted;
+
 	public CountingAdapter(Context context, Counter data) {
 		mContext = context;
 		mData = data;
-	}
-	
-	@Override
-	public int getCount() {
-		return mData.getCells().size();		
+		preferences = PreferenceManager.getDefaultSharedPreferences(context); 
+		showNumbers = preferences
+				.getBoolean(PreferencesActivity.SHOW_NUM, true);
+		showPercents = preferences.getBoolean(PreferencesActivity.SHOW_PERCENT,
+				true);
+
 	}
 
 	@Override
-	public CellButton getItem(int position) {		
+	public int getCount() {
+		return mData.getCells().size();
+	}
+
+	@Override
+	public CellButton getItem(int position) {
 		return mData.getCells().get(position);
 	}
 
 	@Override
-	public long getItemId(int arg0) {		
+	public long getItemId(int arg0) {
 		return 0;
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		final ViewHolder holder;
-        if (convertView == null) {
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.cell_gridview_item, null);
+		if (convertView == null) {
+			convertView = LayoutInflater.from(mContext).inflate(
+					R.layout.cell_gridview_item, null);
 
-            holder = new ViewHolder();
-            holder.name = (TextView) convertView.findViewById(R.id.name);
-            holder.number = (TextView) convertView.findViewById(R.id.number);
-            holder.percent = (TextView) convertView.findViewById(R.id.percent);
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
-        
-        CellButton cell = getItem(position);
-        holder.name.setText(cell.getName());
-        holder.number.setText(cell.getCount() + "");
-        
-        DecimalFormat df = new DecimalFormat("#.##");        
-        if (mData.getTotal() == 0)
-        	holder.percent.setText("0%");
-        else 
-        	holder.percent.setText(String.format(df.format(100.0 * cell.getCount() / mData.getTotal()))  + "%");
-        
-        convertView.setBackgroundResource(cell.getColor());
-        
+			holder = new ViewHolder();
+			holder.name = (TextView) convertView.findViewById(R.id.name);
+			holder.number = (TextView) convertView.findViewById(R.id.number);
+			holder.percent = (TextView) convertView.findViewById(R.id.percent);
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+
+		CellButton cell = getItem(position);
+		holder.name.setText(cell.getName());
+		if (showNumbers) {
+			holder.number.setText(cell.getCount() + "");
+		}else{
+			holder.number.setText("");
+		}
+		DecimalFormat df = new DecimalFormat("#.##");
+		if (showPercents) {
+			if (mData.getTotal() == 0)
+				holder.percent.setText("0%");
+			else
+				holder.percent.setText(String.format(df.format(100.0
+						* cell.getCount() / mData.getTotal()))
+						+ "%");
+		}else{
+			holder.percent.setText("");
+		}
+		convertView.setBackgroundResource(cell.getColor());
+
 		return convertView;
 	}
-	
+
 	private Context mContext;
 	private Counter mData;
-	
+
 	private class ViewHolder {
-        private TextView name;
-        private TextView number;
-        private TextView percent;
-    }
+		private TextView name;
+		private TextView number;
+		private TextView percent;
+	}
 }
