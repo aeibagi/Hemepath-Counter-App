6191f3628f6cd4f76a6142b781b4dc1eab857c8b
jakekarnes42 <jake.karnes@gmail.com>
2013-03-25 16:49:53 -0700

    Added basic email validation

--- src/edu/sjsu/hemepathcounter/PreferencesFragment.java
+++ src/edu/sjsu/hemepathcounter/PreferencesFragment.java
@@ -1,15 +1,48 @@
 package edu.sjsu.hemepathcounter;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceFragment;
+import android.util.Log;
+import android.widget.Toast;
 
 public class PreferencesFragment extends PreferenceFragment {
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// Load the preferences from an XML resource
+		addPreferencesFromResource(R.xml.preferences);
+		
+		
+		//Add email validation
+		OnPreferenceChangeListener listener = new OnPreferenceChangeListener() {
 
-        // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.preferences);
-    }
+			@Override
+			public boolean onPreferenceChange(Preference preference,
+					Object newValue) {
+				if (newValue == null) {
+					return false;
+				} else {
+					String newEmail = (String) newValue;
+					if(isValidEmail(newEmail)){
+						Log.d(getTag(),"Valid email address saved: " + newEmail);
+						return true;
+					}else{
+						Log.d(getTag(),"Invalid email address NOT saved: " + newEmail);
+						Toast.makeText(getActivity(),
+								"Invalid Email Address. Not Saved.",
+								Toast.LENGTH_SHORT).show();
+						return false;
+					}
+				}
+			}
 
+			private boolean isValidEmail(String newValue) {
+				return android.util.Patterns.EMAIL_ADDRESS.matcher(newValue).matches();
+			}
+		};
+		findPreference("pref_email").setOnPreferenceChangeListener(listener);
+	}
 }
